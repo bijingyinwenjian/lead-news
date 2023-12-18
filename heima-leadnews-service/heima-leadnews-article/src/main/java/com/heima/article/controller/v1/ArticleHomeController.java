7ee9001be3ef4f87cc8b902cbc99100b383883cc
yinjiang <yinjiang@exworth.group>
2023-12-18 22:33:58 +0800

    article

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ArticleHomeController.java
@@ -0,0 +1,41 @@
+package com.heima.article.controller.v1;
+
+import com.heima.article.service.ApArticleService;
+import com.heima.common.constants.ArticleConstants;
+import com.heima.model.article.dtos.ArticleHomeDto;
+import com.heima.model.common.dtos.ResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/api/v1/article")
+@Api(tags = "文章列表接口")
+public class ArticleHomeController {
+
+    @Resource
+    private ApArticleService apArticleService;
+
+    @ApiOperation("加载")
+    @PostMapping("/load")
+    public ResponseResult load(@RequestBody ArticleHomeDto dto) {
+        return apArticleService.load(ArticleConstants.LOADTYPE_LOAD_MORE,dto);
+    }
+
+    @ApiOperation("加载更多")
+    @PostMapping("/loadmore")
+    public ResponseResult loadMore(@RequestBody ArticleHomeDto dto) {
+        return apArticleService.load(ArticleConstants.LOADTYPE_LOAD_MORE,dto);
+    }
+
+    @ApiOperation("加载最新")
+    @PostMapping("/loadnew")
+    public ResponseResult loadNew(@RequestBody ArticleHomeDto dto) {
+        return apArticleService.load(ArticleConstants.LOADTYPE_LOAD_NEW,dto);
+    }
+}
